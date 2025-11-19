90fbe2d754a9eff951a11a7e793bf3e6c6048e8f
Dikshant Lama <dixantlama2007@gmail.com>
2025-11-19 12:44:13 +0545

    GitHub

---
+++ Gitbaba.java
@@ -0,0 +1,13 @@
+
+/**
+ * Write a description of class Gitbaba here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Gitbaba
+{
+    public static void main(String[] args){
+        System.out.println("Hello Git");
+    }
+}
